503f91cf147076fbc5461682a78c4511f36ea416
Bookworm-bit <81590955+Bookworm-bit@users.noreply.github.com>
2021-10-26 15:55:31 -0400

    Add files via upload

---
+++ numGenGuess.py
@@ -0,0 +1,18 @@
+import random
+
+parameterSmall = random.randint(0, 10)
+parameterLarge = random.randint(11, 50)
+number = random.randint(parameterSmall, parameterLarge)
+print("Welcome to guess my number! My number is between", parameterSmall, "and", parameterLarge, "Your guess is:")
+guess = input("\n")
+
+while (int(guess) != number):
+    if (int(guess) < number):
+        print("Too small!")
+        guess = input("Try Again!:\n")
+    if (int(guess) > number):
+        print("Too big!")
+        guess = input("Try Again!:\n")
+    
+if (int(guess) == number):
+    print("You are correct! You win!" )
